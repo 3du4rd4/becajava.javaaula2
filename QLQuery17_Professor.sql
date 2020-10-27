edf85048ede801fdc12a1b8cb57f92ef239ab48a
Eduarda Oliveira Filadelfo <eolivefi@everis.com>
2020-10-27 15:57:53 -0300

    Primeiras tabelas'

---
+++ QLQuery17_Professor.sql
@@ -0,0 +1,16 @@
+insert into Professor (ID , Nome) values (1,'Paulo') ;
+insert into Professor (ID , Nome) values (2,'Antonio') ;
+insert into Professor (ID , Nome) values (3,'Marizete') ;
+insert into Professor (ID , Nome) values (4,'Carolina') ;
+insert into Professor (ID , Nome) values (5,'Joaquim') ;
+insert into Professor (ID , Nome) values (6,'Marcos') ;
+insert into Professor (ID , Nome) values (7,'Jorge') ;
+insert into Professor (ID , Nome) values (8,'Estefano') ;
+insert into Professor (ID , Nome) values (9,'Santos') ;
+insert into Professor (ID , Nome) values (10,'Luiza') ;
+SELECT * FROM Professor
+
+DELETE FROM Professor
+
+
+
